ed178c611ace2f85d883f179ba30773d394a0a41
NabaaAbdulhussein <111922494+NabaaAbdulhussein@users.noreply.github.com>
2022-09-22 11:47:54 -0400

    final commit

--- app/src/main/java/edu/temple/assignment2/MainActivity.kt
+++ app/src/main/java/edu/temple/assignment2/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.temple.assignment2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var Password: EditText
     lateinit var PasswordConfrim: EditText
     lateinit var Button: Button
+    lateinit var SpinnerView: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,28 +23,43 @@ class MainActivity : AppCompatActivity() {
 
         //spinner
         val Programs = arrayOf(
+            "Please select your program: ",
             "Information Science",
             "Computer Science",
             "Math and CS",
             "Data Science",
             "Other"
         )
-        val SpinnerProgram = findViewById<Spinner>(R.id.SpinnerView);
-        val ArrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, Programs)
 
-        /*SpinnerProgram.adapter = ArrayAdapter
-        SpinnerProgram.onItemSelectedListener = object :
-            AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        SpinnerView = findViewById<Spinner>(R.id.SpinnerView)
+        Name = findViewById<EditText>(R.id.Name)
+        Email = findViewById<EditText>(R.id.Email)
+        Password = findViewById<EditText>(R.id.Password)
+        PasswordConfrim = findViewById<EditText>(R.id.PasswordConfirm)
 
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, Programs)
+        SpinnerView.adapter = arrayAdapter
+
+        findViewById<Button>(R.id.SaveButton).setOnClickListener {
+            val b = CheckInput()
+            if(b) {
+                performSignUp()
             }
+        }
+    }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
+    //spinner empty
+    private fun setSpinnerError() {
 
-
-        }*/
+        val selectedView: View = SpinnerView.getSelectedView()
+        if (selectedView != null) {
+            SpinnerView.requestFocus()
+            val selectedTextView = selectedView as TextView
+            selectedTextView.error = "error"
+            selectedTextView.setTextColor(Color.RED)
+            SpinnerView.performClick()
+        }
     }
 
     fun Initialization() {
@@ -53,62 +71,57 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    //check whether the fields are empty of not     what way is better??
-    fun IfEmpty() {
-        if (Name.getText().toString().isEmpty() || Email.getText().toString()
-                .isEmpty() || Password.getText().toString().isEmpty() ||
-            PasswordConfrim.getText().toString().isEmpty()
-        ) {
-            Toast.makeText(this, "All Fields Need To Be Answered!", Toast.LENGTH_SHORT).show();
-            //if yes then toast will apear to show the fields are mandatory
-
-
-        }
-    }
-
-
     // Checking if the input is valid
     fun CheckInput(): Boolean {
-        if (Name.text.toString().isEmpty()) { //which one??
-            Name.setError("Please Enter Name")
-            return false
-        }
-        if (Email.text.toString().equals("")) {
-            Email.setError("Please Enter Email")
-            return false
-        }
-        if (Password.text.toString().equals("")) {
-            Password.setError("Please Enter Password")
-            return false
-        }
-        if (PasswordConfrim.text.toString().equals("")) {
-            PasswordConfrim.setError("Please Confirm Password")
-            return false
-        }
+        var nameErr: Boolean = true
+        var emailErr: Boolean = true
+        var passErr: Boolean = true
+        var passErr2: Boolean = true
+        var passMatch: Boolean = true
+        var spinnerErr: Boolean = true
 
+        if (SpinnerView.selectedItemPosition == 0) {
+            spinnerErr = false
+            setSpinnerError()
+
+        }
+        if (Name.text.toString().isNullOrBlank()) {
+            Name.setError("Please Enter Name")
+            nameErr = !nameErr
+        }
+        if (Email.text.toString().isNullOrBlank()) {
+            Email.setError("Please Enter Email")
+//            return false
+            emailErr = !emailErr
+        }
+        if (Password.text.toString().isNullOrBlank()) {
+            Password.setError("Please Enter Password")
+//            return false
+            passErr = !passErr
+        }
+        if (PasswordConfrim.text.toString().isNullOrBlank()) {
+            PasswordConfrim.setError("Please Confirm Password")
+//            return false'
+            passErr2 = !passErr
+        }
         // Checking if repeat password is same
         if (!Password.text.toString().equals(PasswordConfrim.text.toString())) {
             PasswordConfrim.setError("Password does not match!")
-            return false
+            passMatch = !passMatch
         }
-        return true
+        return passMatch and passErr2 and passErr and emailErr and nameErr and spinnerErr
     }
 
-    fun AllValid(view: View) {
+    //print out welcome message
+    //We tried to run this but the pop up message was not showing, you said no points will be reduced
+    fun performSignUp() {
         if (CheckInput()) {
-            // Input is valid
-            val firstName = Name.text.toString()
-            val email = Email.text.toString()
-            val password = Password.text.toString()
-            val CPassword = PasswordConfrim.text.toString()
 
-            //welcome message
-            Toast.makeText(
-                this,
-                "Welcome, " + Name + ", to the SignUpForm App",
-                Toast.LENGTH_LONG
-            ).show();
+            val firstName = Name.text.toString()
+
+            Toast.makeText(applicationContext,"Welcome, " + firstName + ", to the SignUpForm App",Toast.LENGTH_SHORT).show()
 
         }
     }
 }
+
